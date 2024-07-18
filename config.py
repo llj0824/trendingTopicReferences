4c15cc943f86af7595a342f088eee569b6cdda4d
Leo J <llj0824@gmail.com>
2024-07-18 14:12:14 +0800

    added placeholder config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+# Twitter API credentials
+TWITTER_CREDENTIALS = {
+    # "consumer_key": "FILL_IN",
+    # "consumer_secret": "FILL_IN",
+    # "bearer_token": "FILL_IN",
+    # "access_token": "FILL_IN",
+    # "access_token_secret": "FILL_IN"
+}
+
+GOOGLE_YOUTUBE_CREDENTIALS = {
+    # "client_id": "FILL_IN",
+    # "client_secret": "FILL_IN"
+}
+
+LLM_CREDENTIALS = {
+    # "ANTHROPIC_API_KEY": "FILL_IN",
+    # "OPENAI_API_KEY": "FILL_IN" 
+}
